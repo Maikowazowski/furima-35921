361c5b63e9519751824a50fb78a83e8fd15d82be
Maikowazowski <maikot3587@gmail.com>
2021-09-23 12:53:02 +0900

    ActiveHashの作成・バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,21 @@
 class Item < ApplicationRecord
   belongs_ to :user
   
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_fee
+  belongs_to :shipping_from
+  belongs_to :scheduled_delivery
+
   with_options presence: true do
     validates :title 
     validates :description
-    validates :category_id
-    validates :condition_id
-    validates :shipping_fee_id
-    validates :shipping_from_id
-    validates :scheduled_delivery_id
+    validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :shipping_fee_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :shipping_from_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :scheduled_delivery_id, numericality: { other_than: 1, message: "can't be blank" }
   end
 
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is out of setting range'}, format: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters' }

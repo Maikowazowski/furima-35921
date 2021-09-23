a0d6006f01218ffe25ae4d0d46e4d97675f0c071
Maikowazowski <maikot3587@gmail.com>
2021-09-23 13:41:09 +0900

    アクション・ルーティングの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,15 +1,21 @@
 class ItemsController < ApplicationController
-  #before_action :move_to_index, except: [:index]
+  before_action :move_to_index, except: [:index]
 
   def index
   end
+  
+  def new
+  end
 
-  #  private
+  def create
+  end
+  
+  private
 
-  #  def move_to_index
-  #    unless user_signed_in?
-  #      redirect_to action: :index
-  #    end
-  #  end
+  def move_to_index
+   unless user_signed_in?
+   redirect_to action: :index
+   end
+  end
   
 end

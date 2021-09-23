8b808ffa67278392fc354ddd1c48d30765ac008e
Maikowazowski <maikot3587@gmail.com>
2021-09-23 13:56:47 +0900

    ActiveStorageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,13 +9,17 @@ class ItemsController < ApplicationController
 
   def create
   end
-  
+
   private
 
   def move_to_index
    unless user_signed_in?
    redirect_to action: :index
    end
+  end
+
+  def item_params
+    params.require(:item).permit(:image, :title, :description, :category_id, :condition_id, :shipping_fee_id, :shipping_from_id, :scheduled_delivery_id, :price).merge(user_id: current_user.id)
   end
   
 end

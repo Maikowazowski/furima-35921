3ee8690254880c5138e63645ec53ec6e7ad26ae5
Maikowazowski <maikot3587@gmail.com>
2021-10-02 19:56:00 +0900

    indexアクション定義・ビューの修正

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,9 +1,11 @@
 class PurchasesController < ApplicationController
   before_action :authenticate_user!
-  
+
   def index
+    @item = Item.find(params[:item_id])
   end
 
   def create
   end
+
 end

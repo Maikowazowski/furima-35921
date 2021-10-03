7dcb7544ab12fd52d6cb385dfa6ec96e3a4cdcc2
Maikowazowski <maikot3587@gmail.com>
2021-10-03 15:47:58 +0900

    売却済み商品に対するビュー・コントローラーの修正

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -39,7 +39,7 @@ class PurchasesController < ApplicationController
   end
 
   def move_to_index
-    if user_signed_in? && current_user.id == @item.user.id
+    if (user_signed_in? && current_user.id == @item.user.id) || (@item.purchase.present?)
       redirect_to root_path
     end
   end

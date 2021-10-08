02ca1dccae18674ee2a85ca6f3f4d4bd58f4436f
Maikowazowski <maikot3587@gmail.com>
2021-10-08 10:09:26 +0900

    編集削除画面への遷移に関する条件分岐の訂正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -51,9 +51,9 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    unless (user_signed_in? && current_user.id == @item.user.id)
+    unless user_signed_in? && current_user.id == @item.user.id
       redirect_to action: :index
-    else @item.purchase.blank?
+    else @item.purchase.present?
       redirect_to action: :index
     end
   end
